83cd559848a2d66f3e7123bb5b4b64667e6fb200
k3j123 <let1@mymail.nku.edu>
2018-04-08 00:44:04 -0400

    First Commit

---
+++ app/src/main/java/k3j123/minermonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package k3j123.minermonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
